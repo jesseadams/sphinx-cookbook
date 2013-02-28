300294fa96caf7ad0b2735aada17bf539a79b19f
Ryan Schlesinger <ryan@instanceinc.com>
2013-02-27 16:38:17 -0800

    Adding recipes to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.5"
 
 depends "build-essential"
+
+recipe "sphinx", "Installs sphinx"
+recipe "sphinx::package", "Installs sphinx from a package"
+recipe "sphinx::source", "Installs sphinx from source"
