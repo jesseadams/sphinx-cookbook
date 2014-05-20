2e2e62388793c9f1168502abdf31471dc9ba9a1e
Victor Palma <palma.victor@gmail.com>
2014-05-20 18:26:05 -0500

    Fixing typo

--- recipes/rpm.rb
+++ recipes/rpm.rb
@@ -6,6 +6,16 @@ remote_file "#{Chef::Config[:file_cache_path]}/#{sphinx_rpm}" do
   action :create_if_missing
 end
 
+if platform_family?('rhel')
+  %w( unixODBC
+      postgresql-libs
+      mysql-libs).each do |rpm_package|
+        yum_package rpm_package do
+          action :install
+        end
+  end
+end
+
 rpm_package sphinx_rpm do
   source "#{Chef::Config[:file_cache_path]}/#{sphinx_rpm}"
   action :install
