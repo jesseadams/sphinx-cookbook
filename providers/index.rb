d0a0502037ec3d6fdf0c7656295e564d3b889430
Pavel Yudin <pyudin@parallels.com>
2014-09-23 19:59:22 +0400

    Provider fixed

--- providers/index.rb
+++ providers/index.rb
@@ -6,45 +6,42 @@ use_inline_resources if defined?(use_inline_resources)
 
 action :create do
   if node[:sphinx][:install_method] == 'source'
-    conf_path = "#{node[:sphinx][:install_path]}/conf.d/#{new_resource.name}_index.txt"
-    data_path = "#{node[:sphinx][:install_path]}/data/#{new_resource.name}_index"
+    conf_path = "#{node[:sphinx][:source][:install_path]}/conf.d/#{new_resource.name}_index.txt"
+    data_path = "#{node[:sphinx][:source][:install_path]}/data/#{new_resource.name}_index"
   else
     conf_path = "#{node[:sphinx][:package][:conf_path]}/conf.d/#{new_resource.name}_index.txt"
     data_path = "#{node[:sphinx][:package][:data_dir]}/#{new_resource.name}_index"
   end
 
   template conf_path do
-      cookbook "sphinx"
-      source "index.erb"
-      owner node[:sphinx][:user]
-      group node[:sphinx][:group]
-      mode 0755
-      variables :new_resource => new_resource,
-          :data_path => data_path
+    cookbook "sphinx"
+    source "index.erb"
+    owner node[:sphinx][:user]
+    group node[:sphinx][:group]
+    mode 0755
+    variables :new_resource => new_resource,
+        :data_path => data_path
   end
 end
 
-action :reindex do
-    execute "Reindexing #{new_resource.name}" do
-        rotate = new_resource.rotate ? "--rotate" : ""
-        config = "--config #{node[:sphinx][:install_path]}/sphinx.conf"
-        command "indexer #{config} #{rotate} #{new_resource.name}"
-    end
+# action :reindex do
+#     execute "Reindexing #{new_resource.name}" do
+#         rotate = new_resource.rotate ? "--rotate" : ""
+#         config = "--config #{node[:sphinx][:install_path]}/sphinx.conf"
+#         command "indexer #{config} #{rotate} #{new_resource.name}"
+#     end
 
-    new_resource.updated_by_last_action(true)
-end
+#     new_resource.updated_by_last_action(true)
+# end
 
 action :delete do
   if node[:sphinx][:install_method] == 'source'
-    conf_path = "#{node[:sphinx][:install_path]}/conf.d/#{new_resource.name}_index.txt"
+    conf_path = "#{node[:sphinx][:source][:install_path]}/conf.d/#{new_resource.name}_index.txt"
   else
     conf_path = "#{node[:sphinx][:package][:conf_path]}/conf.d/#{new_resource.name}_index.txt"
   end
 
-  execute "Deleting #{new_resource.name}" do
-    command "rm #{conf_path}"
-    notifies :restart, "service[#{node[:sphinx][:package][:daemon]}]"
+  file conf_path do
+    action :delete
   end
-
-  new_resource.updated_by_last_action(true)
 end
