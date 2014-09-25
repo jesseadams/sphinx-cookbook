d53fa933d725b063cea9dcc75164b5f67fa9d6e5
Pavel Yudin <pyudin@parallels.com>
2014-09-25 15:41:28 +0400

    Unused lwrp source deleted

--- resources/source.rb
+++
@@ -1,7 +0,0 @@
-actions :create, :delete
-default_action :create
-
-attribute :name, :kind_of => String, :name_attribute => true, :required => true
-
-attribute :type, :kind_of => String, :required => true
-attribute :params, :kind_of => Hash, :required => true, :default => {}
