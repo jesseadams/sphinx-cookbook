9564f347d7b225d2be9989671c2ec54447766b05
Pavel Yudin <pyudin@parallels.com>
2014-09-25 15:19:56 +0400

    sql_source lwrp added

---
+++ providers/sql_source.rb
@@ -0,0 +1,34 @@
+def whyrun_supported?
+  true
+end
+
+use_inline_resources if defined?(use_inline_resources)
+
+action :create do
+  if node[:sphinx][:install_method] == 'source'
+    conf_path = "#{node[:sphinx][:install_path]}/conf.d/#{new_resource.name}_source.txt"
+  else
+    conf_path = "#{node[:sphinx][:package][:conf_path]}/conf.d/#{new_resource.name}_source.txt"
+  end
+
+  template conf_path do
+    cookbook "sphinx"
+    source "sql_source.erb"
+    owner node[:sphinx][:user]
+    group node[:sphinx][:group]
+    mode 0755
+    variables :new_resource => new_resource
+  end
+end
+
+action :delete do
+  if node[:sphinx][:install_method] == 'source'
+    conf_path = "#{node[:sphinx][:install_path]}/conf.d/#{new_resource.name}_index.txt"
+  else
+    conf_path = "#{node[:sphinx][:package][:conf_path]}/conf.d/#{new_resource.name}_index.txt"
+  end
+
+  file conf_path do
+    action :delete
+  end
+end
---
+++ resources/sql_source.rb
@@ -0,0 +1,35 @@
+actions :create, :delete
+default_action :create
+
+attribute :name, :kind_of => String, :name_attribute => true, :required => true
+attribute :type, :kind_of => String, :required => true, :equal_to => ['mysql', 'pgsql', 'mssql']
+attribute :sql_host, :required => true
+attribute :sql_port, :kind_of => Integer
+attribute :sql_user, :kind_of => String
+attribute :sql_pass, :kind_of => String
+attribute :sql_db, :kind_of => String
+attribute :sql_sock, :kind_of => String
+attribute :mysql_connect_flags, :kind_of => Integer
+attribute :mysql_ssl_cert, :kind_of => String
+attribute :mysql_ssl_key, :kind_of => String
+attribute :mysql_ssl_ca, :kind_of => String
+attribute :sql_query_pre, :kind_of => [String, Array]
+attribute :sql_query, :kind_of => String, :required => true
+attribute :sql_joined_field, :kind_of => [String, Array]
+attribute :sql_query_range, :kind_of => String
+attribute :sql_range_step, :kind_of => Integer
+attribute :sql_query_killlist, :kind_of => String
+attribute :sql_attr_uint, :kind_of => [String, Array]
+attribute :sql_attr_bool, :kind_of => [String, Array]
+attribute :sql_attr_bigint, :kind_of => [String, Array]
+attribute :sql_attr_timestamp, :kind_of => [String, Array]
+attribute :sql_attr_float, :kind_of => [String, Array]
+attribute :sql_attr_multi, :kind_of => [String, Array]
+attribute :sql_attr_string, :kind_of => [String, Array]
+attribute :sql_attr_json, :kind_of => [String, Array]
+attribute :sql_field_string, :kind_of => [String, Array]
+attribute :sql_file_field, :kind_of => String
+attribute :sql_query_post, :kind_of => String
+attribute :sql_query_post_index, :kind_of => String
+attribute :sql_ranged_throttle, :kind_of => Integer
+attribute :mssql_winauth, :kind_of => [TrueClass, FalseClass]
