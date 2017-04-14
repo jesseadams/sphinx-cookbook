3cdfdc1009dd3b473e49d965516b91b564131acb
Denis-Mak <denis.makarskiy@gmail.com>
2017-04-14 15:53:31 +0300

    Changes for compatibility with Chef 12.14. + changes from @reidab PR connecting with mysql migration from recipes to resources

--- resources/index.rb
+++ resources/index.rb
@@ -4,6 +4,6 @@ default_action :reindex
 attribute :name, :kind_of => String, :name_attribute => true, :required => true
 
 attribute :source, :kind_of => String
-attribute :params, :kind_of => Hash, :required => true, :default => {}
+attribute :attrs, :kind_of => Hash, :required => true, :default => {}
 
 attribute :rotate, :kind_of => [TrueClass, FalseClass], :default => false
